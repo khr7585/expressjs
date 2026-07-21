f3cb7baec0814fb43e69982328776c4def0f76c2
Hemanth Reddy <kollahemanthreddy@gmail.com>
2026-07-21 18:10:15 +0530

    chore : completed middlewares

--- phase-2/middleware.js
+++ phase-2/middleware.js
@@ -73,3 +73,81 @@
 // helmet → Adds security-related HTTP headers
 // const cors = require("cors");
 // app.use(cors());
+
+//CUSTOM MIDDLEWARE
+//SYNTAX
+// function middlewarename(req,res,next){
+//     //your custom logic
+//     next();
+// }
+
+//EXAMPLE:LOGGING MIDDLEWARE
+// const express=require("express");
+// const app=express();
+// function middleware(req,res,next){
+//     console.log(`${req.method} ${req.url}`);
+//     next();
+// }
+// app.use(middleware);
+// app.get("/",(req,res)=>{
+//     res.send("welcome");
+// });
+// app.listen(3000,()=>{
+//     console.log("server is running on port 3000");
+// });
+
+//EXAMPLE:AUTHENTICATION MIDDLEWARE
+// const express=require("express");
+// const app=express();
+// function auth(req,res,next){
+//     const isLoggedIn=true;
+//     if(isLoggedIn){
+//         next();
+//     }else{
+//         res.send("access denied");
+//     }
+// }
+// app.get("/dashboard",auth,(req,res)=>{
+//     res.send("dashboard");
+// });
+// app.listen(3000,()=>{
+//     console.log("running")
+// })
+
+//THIRD-PARTY MIDDLEWARE
+// 1.CORS
+//npm install cors
+// const cors=require("cors");
+// app.use(cors());
+
+//2.morgan
+//npm install morgan
+// const morgan = require("morgan");
+// app.use(morgan("dev"));
+
+//3.helmet
+//npm install helmet
+// const helmet = require("helmet");
+// app.use(helmet());
+
+//4.cookie-parser
+//npm install cookie-parser
+// const cookieParser = require("cookie-parser");
+// app.use(cookieParser());
+
+//EXAMPLE
+// const express=require("express");
+// const cors=require("cors");
+// const app=express();
+// app.use(cors());
+// function logger(req,res,next){
+//     console.log("request recived");
+//     next();
+// }
+// app.use(logger);
+// app.get("/",(req,res)=>{
+//     console.log("hello");
+// });
+// app.listen(3000,()=>{
+//     console.log("running");
+// })
