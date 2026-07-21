a39b84155c4e5abc2477937fc024af3096e437d3
Hemanth Reddy <kollahemanthreddy@gmail.com>
2026-07-21 21:20:58 +0530

    feat : learned expree route & organisation

---
+++ phase-3/exp-router.js
@@ -0,0 +1,97 @@
+
+// 15. Controllers
+
+// const { Router } = require("express");
+
+// Folder
+
+// controllers/
+
+// userController.js
+
+// Move logic from routes.
+
+// 16. MVC Pattern
+
+// Understand
+
+// Routes
+
+// ↓
+
+// Controller
+
+// ↓
+
+// Model
+
+// ↓
+
+// Database
+
+
+
+//SYNTAX
+// const express=require("express");
+// const router=express.Router();
+
+//create routes
+// router.get("/",(req,res)=>{
+//     res.send("home page");
+// });
+// router.post("/",(req,res)=>{
+//     res.send("create data");
+// });
+
+//export the router
+// module.exports=router;
+
+//1.Create routes/userroutes.js
+// const express=require("express");
+// const router=express.Router();
+// router.get("/",(req,res)=>{
+//     res.send("all users");
+// });
+// router.get("/:id",(req,res)=>{
+//     res.send(`user id:${req.params.id}`);
+// });
+// module.exports=router;
+
+//COMMON ROUTER METHODS
+// router.get()-handles get requests
+// router.post()-handles post requests
+// router.put()-handles put requests
+// router.patch()-handles patch requests
+// router.delete()-handles delete requests
+// router.use()-applies middleware to router-level routes
+
+
+//ROUTE ORGANISATION
+//1. USER ROUTES
+// const express=require("express");
+// const router=express.Router();
+// router.get("/",(req,res)=>{
+//     res.send("all users");
+// });
+// module.exports=router;
+
+//2.PRODUCT ROUTES
+// const express=require("express");
+// const router=express.Router();
+// router.get("/",(req,res)=>{
+//     res.send("all products");
+// });
+// module.exports=router;
+
+//3.AUTH ROUTES
+// const express=require("express");
+// const router=express.Router();
+// router.post("/login",(req,res)=>{
+//     res.send("user login");
+// });
+// router.post("/register",(req,res)=>{
+//     res.send("user register");
+// });
+// module.exports=router;
+
+//connected routes in export.js
---
+++ phase-3/export.js
@@ -0,0 +1,19 @@
+// const express=require("express");
+// const app=express();
+// const userroutes=require("./exp-router");
+// app.use("/users",userroutes);
+// app.listen(3000,()=>{
+//     console.log("server running");
+// });
+
+// const express=require("express");
+// const app=express();
+// const userroutes=require("./exp-router");
+// const productroutes=require("./exp-router");
+// const authroutes=require("./exp-router");
+// app.use("/users",userroutes);
+// app.use("/products",productroutes);
+// app.use("/auth",authroutes);
+// app.listen(3000,()=>{
+//     console.log("server running");
+// });
